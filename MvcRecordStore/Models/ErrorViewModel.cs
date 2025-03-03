b3e04e648c16950a9e0ddcb6b353cc2b60d88e77
TinyPuff <ghost1of1king@gmail.com>
2025-03-03 17:37:11 +0330

    initial commit

---
+++ MvcRecordStore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcRecordStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
